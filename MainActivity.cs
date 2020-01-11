cffaba76566a90234e83112bd1c16e8a9ee57330
hemed1 <hemedmeir@gmail.com>
2020-01-11 19:25:29 +0200

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using ProjTaskRemindet.Utils;
+
+
+namespace ProjTaskReminder
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        ListView simpleList;
+        private readonly string[]  countryList = new string[6] { "India", "China", "australia", "Portugle", "America", "NewZealand" };
+
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            simpleList = (ListView)FindViewById(Resource.Id.simpleListView);
+
+            FillList();
+        }
+
+        [Obsolete]
+        private void FillList()
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < countryList.Length; ++i)
+            {
+                list.Add(countryList[i]);
+            }
+            
+            ListViewAdapter listViewAdapter = new ListViewAdapter(this.ApplicationContext, list);
+            simpleList.SetAdapter(listViewAdapter);
+
+            //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, Resource.Layout.list_item, Resource.Id.txtTitle, countryList);
+            //simpleList.SetAdapter(arrayAdapter);
+
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
+
---
+++ Utils/ListViewAdapter.cs
@@ -0,0 +1,106 @@
+﻿//package ;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using static Android.Support.V7.Widget.RecyclerView;
+using static Android.Support.V7.Widget.CardView;
+using ProjTaskRemindet;
+
+//using Android.App.Activity;
+//using Android.Content;
+//using Android.
+
+namespace ProjTaskRemindet.Utils
+{
+    public class ListViewAdapter : BaseAdapter
+    {
+        private readonly Context context;
+        //private MainActivity mainActivity;
+        //private List<Task> TaskList;
+        private readonly List<string> TaskList;
+
+        //private Resources resources;
+        //private int selectMode = 0;
+        //private int selectedItemsCount;
+
+        private LayoutInflater layoutInflater;
+
+        public override int Count => 21;
+
+        
+
+        public ListViewAdapter(Context applicationContext, List<string> itemsList)
+        {
+            this.context = applicationContext;
+            this.TaskList = itemsList;
+
+            layoutInflater = LayoutInflater.From(this.context);
+            //layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        
+        }
+
+        //public  int Count()
+        //{
+        //    return 2;
+        //}
+
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            //View view = layoutInflater.Inflate(ProjTaskReminder.Resource.Layout.list_item, null);
+
+            //view.FindViewById(R.id.gv_players);
+
+            return null;    // view;    // TaskList[position];  // null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;    //TaskList[position];
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            //if (position >= TaskList.Count)
+            //{
+            //    return convertView;
+            //}
+
+            //layoutInflater = LayoutInflater.From(this.context);
+
+            if (convertView == null)
+            {
+                convertView = layoutInflater.Inflate(ProjTaskReminder.Resource.Layout.list_item, parent, false);
+            }
+
+            if (position < TaskList.Count)
+            {
+                TextView txtTitle = (TextView)convertView.FindViewById(ProjTaskReminder.Resource.Id.txtTitle);
+                txtTitle.SetText(TaskList[position], TextView.BufferType.Normal);
+            }
+
+
+            //ImageView icon = (ImageView)convertView.FindViewById(Resource.Id.icon);
+            //icon.setImageResource(TaskList[i]);
+
+            return convertView;
+        }
+
+        public  int Count2()  // override
+        {
+            return TaskList.Count;
+        }
+    }
+
+        
+    }
